067feb5f644b6d6cc2398eb3f223933103d8628b
huybang2017 <ndhbang2017@gmail.com>
2023-03-19 16:01:20 +0700

    fixing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Page from "./baitap1/Page";
+import ShopGlass from "./baitap2/ShopGlass";
+// import Page from "./baitap1/Page";
 
 function App() {
   return (
     <div>
-      <Page />
+      <ShopGlass />
     </div>
   );
 }
---
+++ src/baitap2/ShopGlass.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+
+function ShopGlass() {
+  return <div>
+    <Header/>
+    <Footer/>
+  </div>;
+}
+
+export default ShopGlass;
